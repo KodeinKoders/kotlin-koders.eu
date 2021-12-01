dd0aa214caf29142ca080d77a2fba49133aa9586
Romain Boisselle <romain@boisselle.dev>
2021-12-01 10:52:20 +0100

    update the schedule

--- data/src/commonMain/kotlin/eu/koders/data/schedule.kt
+++ data/src/commonMain/kotlin/eu/koders/data/schedule.kt
@@ -25,22 +25,22 @@ data class Schedule(
             Slot(15, 20, Talk.ReliableWebServices),
             Slot(16, 20, Talk.Break),
             Slot(16, 50, Talk.GradleKotlinDSL),
-            Slot(17, 20, Talk.CompilerLies)
+            Slot(17, 20, Talk.BridgingKotlinAndSwift)
         ))
 
         val loft by Schedule(listOf(
             Slot(9, 20, Talk.KotlinAdoptionAtScale),
             Slot(10, 20, Talk.KMPLightningNode),
             Slot(10, 50, Talk.Break),
-            Slot(11, 20, Talk.ArrowMetaSerialization),
-            Slot(12, 20, Talk.FunctionalInSpringBoot),
-            Slot(13, 20, Talk.CICDWithKotlin),
-            Slot(13, 50, Talk.QuickStartKMP),
-            Slot(14, 20, Talk.ExploringAdvancedCompose),
+            Slot(11, 20, Talk.FunctionalInSpringBoot),
+            Slot(12, 20, Talk.CICDWithKotlin),
+            Slot(12, 50, Talk.QuickStartKMP),
+            Slot(13, 20, Talk.LongBreak),
+            Slot(14, 20, Talk.KotlinNativeFromTheTrenches),
             Slot(15, 20, Talk.AnimatingCompose),
             Slot(15, 50, Talk.Break),
-            Slot(16, 20, Talk.KotlinNativeFromTheTrenches),
-            Slot(17, 20, Talk.BridgingKotlinAndSwift)
+            Slot(16, 20, Talk.ExploringAdvancedCompose),
+            Slot(17, 20, Talk.CompilerLies)
         ))
     }
 }
